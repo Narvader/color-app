0941f0537ffa8f6db217d7134cc7c8fc6769bef3
Pablo Narvaez <pnarvaez@elconfidencial.com>
2020-11-08 16:49:54 +0100

    created react app, added palette

---
+++ src/Palette.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+    render(){
+        return(
+            <div className="Palette">
+                {/* NAvbar goes here */}
+                <div className="Palette-colors">
+                    {/* Bunch of colors */}
+                </div>
+                {/* footer */}
+            </div>
+        )
+    }
+}
+
+export default Palette;
