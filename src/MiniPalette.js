ede76710865b3b3b1437cbb3a9a0c0f741b959b1
Pablo Narvaez <pnarvaez@elconfidencial.com>
2020-11-12 21:40:51 +0100

    Finish styling MiniPalette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -2,36 +2,65 @@ import React from 'react';
 import { withStyles } from "@material-ui/styles";
 
 const styles = {
-    main: {
-        backgroundColor: "purple",
-        border: "3px solid teal"
-        
-    },
-    secondary: {
-        backgroundColor: "pink",
-        "& h1": {
-            color: "red",
-            "& span": {
-                backgroundColor: "lime"
-            }
+    root: {
+        backgroundColor: "white",
+        border: "1px solid black",
+        borderRadius: "5px",
+        padding: ".5rem",
+        position: "relative",
+        overflow: "hidden",
+        "&:hover": {
+            cursor: "pointer"
         }
+    },
+    colors: {
+        backgroundColor: "#dae1a4",
+        height: "150px",
+        width: "100%",
+        borderRadius: "5px",
+        overflow: "hidden"
+    },
+    title: {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        margin: "0",
+        color: "black",
+        paddingTop: ".5px",
+        fontSize: "1rem",
+        position: "relative"
+    },
+    emoji: {
+        marginLeft: ".5rem",
+        fontSize: "1.5rem"
+    },
+    miniColor: {
+        height: "25%",
+        width: "20%",
+        display: "inline-block",
+        margin: "0 auto",
+        position: "relative",
+        marginBottom: "-3.5px"
     }
 }
 
 function MiniPalette(props) {
-    const {classes} = props;
+    const {classes, paletteName, emoji, colors} = props;
+    const miniColorBoxes = colors.map(color => (
+        <div 
+            className={classes.miniColor} 
+            style={{backgroundColor: color.color}}
+            key={color.name}
+        ></div>
+    ));
     console.log(classes)
     return (
         
-        <div className={classes.main}>
-            <h1>MiniPalette</h1>
-            <span>ddewedwfew</span>
-            <section className={classes.secondary}>
-                <h1>MiniPalette
-                <span> ddewedwfew</span>
-                </h1>
-                
-            </section>
+        <div className={classes.root}>
+            <div className={classes.colors}>
+                {miniColorBoxes}
+            </div>
+            <h5 className={classes.title}>{paletteName} <span className={classes.emoji}>{emoji}</span></h5>
         </div>
     )
 }
